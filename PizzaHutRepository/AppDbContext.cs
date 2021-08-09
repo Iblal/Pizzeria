babea76383358dd0ea7968220afbd48c13746dc4
Iblal <81813762+Iblal@users.noreply.github.com>
2021-08-09 20:52:16 +0100

    added Application DbContext class to project

---
+++ PizzaHutRepository/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PizzaHutEntities;
+
+namespace PizzaHutRepository
+{
+    public class AppDbContext : IdentityDbContext<User, Role, int>
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemType> ItemTypes { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<PaymentDetails> PaymentDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+            if (!optionsBuilder.IsConfigured)
+            {
+                
+
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PizzaHut;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+
+
+    }
+}
--- PizzaHutRepository/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PizzaHutRepository
-{
-    public class Class1
-    {
-    }
-}
