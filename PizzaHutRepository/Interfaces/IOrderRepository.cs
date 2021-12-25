5e67dce606ef55f960a93acd06f58f70a4283ef6
Iblal <81813762+Iblal@users.noreply.github.com>
2021-12-25 17:42:07 +0000

    Added Order repository with implementation and interfaces.

---
+++ PizzaHutRepository/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using PizzaHutEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaHutRepository.Interfaces
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetUserOrders(int UserId);
+    }
+}
