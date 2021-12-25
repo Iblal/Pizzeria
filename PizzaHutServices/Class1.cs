aef15f454cf6216fb9f3dafd2512cf7321e0ab48
Iblal <81813762+Iblal@users.noreply.github.com>
2021-12-25 20:29:49 +0000

    Added authentication service with identity.

--- PizzaHutServices/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PizzaHutServices
-{
-    public class Class1
-    {
-    }
-}
---
+++ PizzaHutServices/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,20 @@
+﻿using PizzaHutEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaHutServices.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        public interface IAuthenticationService
+        {
+            public bool CreateUser(User user, string Password);
+            public Task<bool> SignOut();
+            public User AuthenticateUser(string Username, string Password);
+            public User GetUser(string Username);
+        }
+    }
+}
