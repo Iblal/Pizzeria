b3d444282564a8aca6c46bb91f75437cc5e9e598
Iblal <81813762+Iblal@users.noreply.github.com>
2021-12-25 17:29:26 +0000

    Added cart repository with both implementation and interface.

---
+++ PizzaHutRepository/Interfaces/ICartRepository.cs
@@ -0,0 +1,19 @@
+﻿using PizzaHutEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaHutRepository.Interfaces
+{
+
+    public interface ICartRepository : IRepository<Cart>
+    {
+        public Cart GetCart(Guid CartId);
+        public int DeleteItem(Guid cartId, int itemId);
+        public int UpdateQuantity(Guid cartId, int itemId, int Quantity);
+        public int UpdateCart(Guid cartId, int userId);
+    }
+
+}
